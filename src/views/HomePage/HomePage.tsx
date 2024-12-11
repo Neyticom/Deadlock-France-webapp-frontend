52e6cb17a87154dea4029478ed6a8d525e8d7364
NPanisson <panisson.nils@gmail.com>
2024-12-11 15:36:23 +0100

    titre + lien patchnotes

--- src/views/HomePage/HomePage.tsx
+++ src/views/HomePage/HomePage.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../layouts/Header/Header';
 import Footer from '../../layouts/Footer/Footer';
+import { Link } from 'react-router-dom';
 import './HomePage.scss';
 // import logo from '/src/assets/icons/logo.png';
 
 const HomePage: React.FC = () => {
+    const [activePage, setActivePage] = useState(window.location.pathname);
+
+    const changePage = () => {
+        setTimeout(() => {
+            setActivePage(window.location.pathname);
+        });
+    }
     return (
         <div className="homepage-container">
             <Header />
             <main className="homepage-main">
                 {/* <img src={logo} alt="Logo Deadlock France" className="logo" /> */}
                 <h1 className="main-title">
-
-                    {/* <span className='main-title_top'>Rejoins vite la communauté</span>
-                    <span className='main-title_bottom'>Deadlock France</span> */}
+                    <span>Dernières mises à jour</span>  
                 </h1>
+
+                <div className='news-container'>
+                    <Link to="/patchnotes" onClick={changePage} className={activePage === '/patchnotes' ? 'active nav-link' : 'nav-link'} id="patchnotes-link">Consulter la liste des patchnotes</Link>
+                </div>
+                {/* <Link to="/" onClick={changePage}></Link> */}
             </main>
             <Footer/>
         </div>
