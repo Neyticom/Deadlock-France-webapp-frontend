6a5c99c44424f6a26dc6e32e17256e7c0a3fdec0
Romain B. (Neyticom) <romainb.boitepro@gmail.com>
2025-01-13 12:42:06 +0100

    feat: patchnotes router & responsive improvements

--- src/views/PatchnotesPage/PatchnotesPage.tsx
+++ src/views/PatchnotesPage/PatchnotesPage.tsx
@@ -5,8 +5,11 @@ import PatchnotesSelector from '../../components/PatchnotesSelector/PatchnotesSe
 import { patchnotes } from '../../assets/data';
 import './PatchnotesPage.scss';
 import { PatchnotesReader } from '../../components/PatchnotesReader/PatchnotesReader';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 
-const [isMobile, setIsMobile] = useState(false);
+const PatchnotesPage = () => {
+
+    const [isMobile, setIsMobile] = useState(true);
 
     useEffect(() => {
         const mediaQuery = window.matchMedia('(max-width: 768px)');
@@ -20,10 +23,18 @@ const [isMobile, setIsMobile] = useState(false);
         return () => mediaQuery.removeEventListener('change', handleResize);
     }, []);
 
-const PatchnotesPage = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        if(!isMobile && location.pathname == '/patchnotes'){
+            navigate('/patchnotes/1');
+        }
+    }, [isMobile, location.pathname, navigate]);
 
     const [activePatchnote, setActivePatchnote] = useState<number | null>(null);
     const [lastestPatchnote, setLastestPachnote] = useState<number | null>(null);
+    const selectedPatchnote = patchnotes.find(patchnote => patchnote.id === activePatchnote);
 
     useEffect(() => {
         const sortedPatchnotes = [...patchnotes].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
@@ -35,29 +46,38 @@ const PatchnotesPage = () => {
         setActivePatchnote(id);
     }
 
-    const selectedPatchnote = patchnotes.find(patchnote => patchnote.id === activePatchnote);
-
     return (
         <div className="patchnotes-page">
-            <Header />
-            <main className='patchnotes-page_content'>
-            {isMobile && (
-                <>
-                <h1>Patchnotes</h1>
-                
-                </>
-            )}
+            <Routes>
+                <Route path="/" element={
+                    <>
+                        <Header />
+                        <main className='patchnotes-page_content'>
+                            <PatchnotesSelector patchnotes={patchnotes} activePatchnote={activePatchnote} handleSelectedPatchnote={handleSelectedPatchnote}/>
+                        </main>
+                        <Footer />
+                    </>
+                }/>
+                <Route path=":params" element={
+                    <>
+                        <Header />
+                        <main className='patchnotes-page_content'>
+                            {selectedPatchnote ? (
+                                <h1 className='patchnotes-page_title'>{lastestPatchnote === activePatchnote ? `Dernière Patchnote - ${selectedPatchnote.date}` : `Patchnote du ${selectedPatchnote.date}` }</h1>
+                            ) : null}
 
-            {selectedPatchnote ? (
-                <h1 className='patchnotes-page_title'>{lastestPatchnote === activePatchnote ? `Dernière Patchnote - ${selectedPatchnote.date}` : `Patchnote du ${selectedPatchnote.date}` }</h1>
-            ) : null}
-           
-                <PatchnotesSelector patchnotes={patchnotes} activePatchnote={activePatchnote} handleSelectedPatchnote={handleSelectedPatchnote}/>
-                {selectedPatchnote ? (
-                    <PatchnotesReader patchnote={selectedPatchnote} activePatchnote={activePatchnote} handleSelectedPatchnote={handleSelectedPatchnote} />
-                ) : null}
-            </main>
-            <Footer />
+                            {!isMobile && (
+                                <PatchnotesSelector patchnotes={patchnotes} activePatchnote={activePatchnote} handleSelectedPatchnote={handleSelectedPatchnote}/>
+                            )}
+
+                            {selectedPatchnote ? (
+                                <PatchnotesReader patchnote={selectedPatchnote} activePatchnote={activePatchnote} handleSelectedPatchnote={handleSelectedPatchnote} />
+                            ) : null}
+                        </main>
+                        <Footer />
+                    </>
+                }/>
+            </Routes>
         </div>
     );
 
